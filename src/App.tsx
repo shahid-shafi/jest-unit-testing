e55350ed6cf191668483938dbde4ffdcf7a205fd
shahid-shafi <shahidshafi.org@gmail.com>
2023-08-10 23:45:06 +0530

    remove - unused imports and variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import Login from './components/Login';
 
 function App() {
-  const a = 100,
-    b = 200;
   return (
     <div className="App">
       <Login />
